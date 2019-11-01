cc00473f0ce02d52acaf49bd7cc1823665bacfff
David Hassell <d.c.hassell@reading.ac.uk>
2019-11-01 10:47:26 +0000

    v3.0.3

--- cf/__init__.py
+++ cf/__init__.py
@@ -79,8 +79,8 @@ except ImportError as error1:
 
 __Conventions__  = 'CF-1.7'
 __author__       = 'David Hassell'
-__date__         = '2019-11-01'
-__version__      = '3.0.3'
+__date__         = 'Not yet released'
+__version__      = '3.0.4'
 __cf_version__   = cfdm.core.__cf_version__
 
 from distutils.version import LooseVersion
