3dc39e254dc896cf14d7ee6faf308cc570a75879
David Hassell <david.hassell@ncas.ac.uk>
2020-11-02 17:53:24 +0000

    docstrings

--- cf/umread_lib/umfile.py
+++ cf/umread_lib/umfile.py
@@ -258,11 +258,16 @@ class Rec:
         '''Default instantiation, which stores the supplied headers and
     offsets.
 
-    The *file* argument, used to set the `!file` attribute, does not
-    need to be supplied, but if it is not then it will have to be set
-    on the returned object (to the containing `File` object) before
-    calling `get_data` will work. Normally this would be done by the
-    calling code instantiating via `File` rather than directly.
+    :Parameters:
+
+        file: `File`, optional
+            Used to set the `!file` attribute. Does not need to be
+            supplied, but if it is not then it will have to be set on
+            the returned `Rec` object before calling `get_data` will
+            work. If set it should be set to the `File` object that
+            contains the returned `Rec` object. Normally this would be
+            done by the calling code instantiating via `File` rather
+            than directly.
 
         '''
         self.int_hdr = int_hdr
@@ -275,14 +280,30 @@ class Rec:
             self.file = file
 
     @classmethod
-    def from_file_and_offsets(cls, file, hdr_offset, data_offset, disk_length):
+    def from_file_and_offsets(cls, file, hdr_offset, data_offset,
+                              disk_length):
         '''Instantiate a `Rec` object from the `File` object and the header
     and data offsets.
 
     The headers are read in, and also the record object is ready for
     calling `get_data`.
 
-    :Returns:
+    :Parameters:
+
+        file: `File`
+            A view of a file including sets of PP records combined
+            into variables.
+
+        hdr_offset: `int`
+            The start word in the file of the header.
+
+        data_offset: `int`
+            The start word in the file of the data.
+
+        disk_length: `int`
+            The length in words of the data in the file.
+
+   :Returns:
 
         `Rec`
 
@@ -296,7 +317,9 @@ class Rec:
         )
 
         return cls(
-            int_hdr, real_hdr, hdr_offset, data_offset, disk_length, file=file)
+            int_hdr, real_hdr, hdr_offset, data_offset, disk_length,
+            file=file
+        )
 
     def read_extra_data(self):
         '''Read the extra data associated with the record.
