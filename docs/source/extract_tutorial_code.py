7fdb6412460ef32212911093a05f452de8a6650f
David Hassell <david.hassell@ncas.ac.uk>
2020-12-17 11:04:45 +0000

    fixed loop

--- docs/source/extract_tutorial_code.py
+++ docs/source/extract_tutorial_code.py
@@ -1,59 +1,69 @@
-'''Extract tutorial python code into an executable python script.
+'''Extract tutorial python code into a executable python scripts.
 
 '''
 import re
+import sys
 
-tutorial = []
-code_block = False
-caption = False
+if len(sys.argv) >= 2:
+    tutorials_rst = sys.argv[1:]
+else:
+    raise ValueError(
+        "Must provide at least one .rst file contaiing code examples"
+    )
 
-with open('tutorial.rst', mode='r') as f:
-    for line in f.readlines():
-        line = line.strip()
+for tutorial_rst in tutorials_rst:
+    tutorial = []
+    code_block = False
+    caption = False
 
-        code = re.split('^\s*>>>\s', line)
-
-        if len(code) == 2:
-            if re.findall('# Raises Exception\s*$', line):
-                code[1] = 'try:\n    ' + code[1]+ '\nexcept:\n    pass'
-        else:
-            code = re.split('^\s*\.\.\.\s', line)
-
-        if len(code) == 2:
-            tutorial.append(code[1])
-            continue
-
-        if re.match('\s*\.\. Code Block Start', line):
-            code_block = True
-            tutorial.append('# Start of code block')
-            continue
-
-        if code_block:
-            if re.match('\s*\.\. code-block::', line):
-                continue
+    with open(tutorial_rst, mode='r') as f:
+        for line in f.readlines():
+            line = line.strip()
             
-            if not caption and re.match('\s*:caption:', line):
-                caption = True
-                continue
-
-            if caption:
-                # Blank line marks end of caption
-                if re.match('\s*$', line):
-                    caption = False
-
-                continue
-                    
-            if re.match('\.\. Code Block End', line):
-                code_block = False
-                tutorial.append('# End of code block')
-                continue
+            code = re.split('^\s*>>>\s', line)
             
-            tutorial.append(line.replace('   ', '', 1))
-# --- End: with
+            if len(code) == 2:
+                if re.findall('# Raises Exception\s*$', line):
+                    code[1] = 'try:\n    ' + code[1]+ '\nexcept:\n    pass'
+            else:
+                code = re.split('^\s*\.\.\.\s', line)
+    
+            if len(code) == 2:
+                tutorial.append(code[1])
+                continue
 
-tutorial.append('')
-            
-with open('tutorial.py', 'r+') as f:
-    f.seek(0)
-    f.write('\n'.join(tutorial))
-    f.truncate()
+            if re.match('\s*\.\. Code Block Start', line):
+                code_block = True
+                tutorial.append('# Start of code block')
+                continue
+    
+            if code_block:
+                if re.match('\s*\.\. code-block::', line):
+                    continue
+                
+                if not caption and re.match('\s*:caption:', line):
+                    caption = True
+                    continue
+                
+                if caption:
+                    # Blank line marks end of caption
+                    if re.match('\s*$', line):
+                        caption = False
+    
+                    continue
+                        
+                if re.match('\.\. Code Block End', line):
+                    code_block = False
+                    tutorial.append('# End of code block')
+                    continue
+                
+                tutorial.append(line.replace('   ', '', 1))
+    # --- End: with
+
+    tutorial.append('')
+
+    tutorial_py = tutorial_rst.replace('.rst', '.py')
+     
+    with open(tutorial_py, 'w') as f:
+        f.write('\n'.join(tutorial))
+# --- End: for
