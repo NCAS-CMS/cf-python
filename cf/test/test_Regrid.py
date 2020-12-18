542d3ebaa9dcd97a787d23e3f8d1c16540d11544
David Hassell <david.hassell@ncas.ac.uk>
2020-12-18 11:43:02 +0000

    regrid test reformat

--- cf/test/test_Regrid.py
+++ cf/test/test_Regrid.py
@@ -28,19 +28,7 @@ class RegridTest(unittest.TestCase):
     filename10 = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               'regrid_file8.nc')
 
-#    f1 = cf.read(filename1)[0]
-#    f2 = cf.read(filename2)[0]
-#    f3 = cf.read(filename3)[0]
-#    f4 = cf.read(filename4)[0]
-#    f5 = cf.read(filename5)[0]
-#    f6 = cf.read(filename6)[0]
-#    f7 = cf.read(filename7)[0]
-#    f8 = cf.read(filename8)[0]
-#    f9 = cf.read(filename9)[0]
-#    f10 = cf.read(filename10)[0]
-
     chunk_sizes = (300, 10000, 100000)[::-1]
-    original_chunksize = cf.chunksize()
 
     test_only = []
 #    test_only = ('NOTHING!!!!!',)
@@ -53,116 +41,134 @@ class RegridTest(unittest.TestCase):
     def test_Field_regrids(self):
         if self.test_only and inspect.stack()[0][3] not in self.test_only:
             return
+        self.assertFalse(cf.regrid_logging())
+        with cf.atol(1e-12):
+            for chunksize in self.chunk_sizes:
+                with cf.chunksize(chunksize):
+                    f1 = cf.read(self.filename1)[0]
+                    f2 = cf.read(self.filename2)[0]
+                    f3 = cf.read(self.filename3)[0]
 
-        # Set tolerance for array equality
-        original_atol = cf.atol(1e-12)
+                    r = f1.regrids(f2, 'conservative')
 
-        for chunksize in self.chunk_sizes:
-            cf.chunksize(chunksize)
-            f1 = cf.read(self.filename1)[0]
-            f2 = cf.read(self.filename2)[0]
-            f3 = cf.read(self.filename3)[0]
+                    self.assertTrue(
+                        f3.equals(r),
+                        "destination=global Field, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+                    r = f1.regrids(f2, method='conservative')
 
-            r = f1.regrids(f2, 'conservative')
+                    self.assertTrue(
+                        f3.equals(r),
+                        "destination=global Field, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+                    dst = {'longitude': f2.dim('X'), 'latitude': f2.dim('Y')}
+                    r = f1.regrids(dst, 'conservative', dst_cyclic=True)
 
-            self.assertTrue(
-                f3.equals(r, verbose=2),
-                'destination = global Field, CHUNKSIZE = %s' % chunksize
-            )
-            r = f1.regrids(f2, method='conservative')
+                    self.assertTrue(
+                        f3.equals(r),
+                        "destination=global dict, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+                    r = f1.regrids(dst, method='conservative', dst_cyclic=True)
+                    self.assertTrue(
+                        f3.equals(r),
+                        "destination=global dict, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
 
-            self.assertTrue(
-                f3.equals(r, verbose=2),
-                'destination = global Field, CHUNKSIZE = %s' % chunksize
-            )
-            dst = {'longitude': f2.dim('X'), 'latitude': f2.dim('Y')}
-            r = f1.regrids(dst, 'conservative', dst_cyclic=True)
+                    f4 = cf.read(self.filename4)[0]
+                    f5 = cf.read(self.filename5)[0]
 
-            self.assertTrue(
-                f3.equals(r, verbose=2),
-                'destination = global dict, CHUNKSIZE = %s' % chunksize
-            )
-            r = f1.regrids(dst, method='conservative', dst_cyclic=True)
-            self.assertTrue(
-                f3.equals(r, verbose=2),
-                'destination = global dict, CHUNKSIZE = %s' % chunksize
-            )
+                    r = f1.regrids(f5, 'linear')
+                    self.assertTrue(
+                        f4.equals(r),
+                        "destination=regional Field, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
 
-            f4 = cf.read(self.filename4)[0]
-            f5 = cf.read(self.filename5)[0]
+                    r = f1.regrids(f5, method='linear')
+                    self.assertTrue(
+                        f4.equals(r),
+                        "destination=regional Field, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+            # --- End: for
 
-            r = f1.regrids(f5, 'linear')
-            self.assertTrue(
-                f4.equals(r, verbose=2),
-                'destination = regional Field, CHUNKSIZE = %s' % chunksize
-            )
-
-            r = f1.regrids(f5, method='linear')
-            self.assertTrue(
-                f4.equals(r, verbose=2),
-                'destination = regional Field, CHUNKSIZE = %s' % chunksize
-            )
-        # --- End: for
-        cf.chunksize(self.original_chunksize)
-
-        f6 = cf.read(self.filename6)[0]
-        with self.assertRaises(Exception):
-            f1.regridc(f6, axes='T', method='linear')
-
-        cf.atol(original_atol)
+            f6 = cf.read(self.filename6)[0]
+            with self.assertRaises(Exception):
+                f1.regridc(f6, axes='T', method='linear')
 
     @unittest.skipUnless(cf._found_ESMF, "Requires esmf package.")
     def test_Field_regridc(self):
         if self.test_only and inspect.stack()[0][3] not in self.test_only:
             return
-
-        original_atol = cf.atol(1e-12)
-
-        for chunksize in self.chunk_sizes:
-            cf.chunksize(chunksize)
-            f1 = cf.read(self.filename7)[0]
-            f2 = cf.read(self.filename8)[0]
-            f3 = cf.read(self.filename9)[0]
-            self.assertTrue(
-                f3.equals(f1.regridc(
-                    f2, axes='T', method='linear'), verbose=2),
-                'destination = time series, CHUNKSIZE = %s' % chunksize
-            )
-            f4 = cf.read(self.filename1)[0]
-            f5 = cf.read(self.filename2)[0]
-            f6 = cf.read(self.filename10)[0]
-            self.assertTrue(
-                f6.equals(f4.regridc(
-                    f5, axes=('X', 'Y'), method='conservative'), verbose=2),
-                'destination = global Field, CHUNKSIZE = %s' % chunksize
-            )
-            self.assertTrue(
-                f6.equals(f4.regridc(
-                    f5, axes=('X', 'Y'), method='conservative'), verbose=2),
-                'destination = global Field, CHUNKSIZE = %s' % chunksize
-            )
-            dst = {'X': f5.dim('X'), 'Y': f5.dim('Y')}
-            self.assertTrue(
-                f6.equals(
-                    f4.regridc(dst, axes=('X', 'Y'), method='conservative'),
-                    verbose=2
-                ),
-                'destination = global dict, CHUNKSIZE = %s' % chunksize
-            )
-            self.assertTrue(
-                f6.equals(
-                    f4.regridc(dst, axes=('X', 'Y'), method='conservative'),
-                    verbose=2
-                ),
-                'destination = global dict, CHUNKSIZE = %s' % chunksize
-            )
-        # --- End: for
-        cf.chunksize(self.original_chunksize)
-
-        cf.atol(original_atol)
-
+        self.assertFalse(cf.regrid_logging())
+        with cf.atol(1e-12):
+            for chunksize in self.chunk_sizes:
+                self.assertFalse(cf.regrid_logging())
+                with cf.chunksize(chunksize):
+                    f1 = cf.read(self.filename7)[0]
+                    f2 = cf.read(self.filename8)[0]
+                    f3 = cf.read(self.filename9)[0]
+                    self.assertTrue(
+                        f3.equals(
+                            f1.regridc(f2, axes='T', method='linear')
+                        ),
+                        "destination=time series, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+                    f4 = cf.read(self.filename1)[0]
+                    f5 = cf.read(self.filename2)[0]
+                    f6 = cf.read(self.filename10)[0]
+                    self.assertTrue(
+                        f6.equals(
+                            f4.regridc(f5, axes=('X', 'Y'),
+                                       method='conservative')
+                        ),
+                        "destination=global Field, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+                    self.assertTrue(
+                        f6.equals(
+                            f4.regridc(f5, axes=('X', 'Y'),
+                                       method='conservative')
+                        ),
+                        "destination=global Field, CHUNKSIZE={}".format(
+                            chunksize
+                            )
+                    )
+                    dst = {'X': f5.dim('X'), 'Y': f5.dim('Y')}
+                    self.assertTrue(
+                        f6.equals(
+                            f4.regridc(dst, axes=('X', 'Y'),
+                                       method='conservative')
+                        ),
+                        "destination=global dict, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
+                    self.assertTrue(
+                        f6.equals(
+                            f4.regridc(dst, axes=('X', 'Y'),
+                                       method='conservative')
+                        ),
+                        "destination=global dict, CHUNKSIZE={}".format(
+                            chunksize
+                        )
+                    )
 
 # --- End: class
+
 
 if __name__ == "__main__":
     print('Run date:', datetime.datetime.now())
