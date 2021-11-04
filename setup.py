0ea55e60dc89f559e389f915b36efb4efcdce82a
David Hassell <davidhassell@users.noreply.github.com>
2021-11-04 12:05:59 +0000

    Drop Python 3.6

--- setup.py
+++ setup.py
@@ -298,7 +298,6 @@ setup(
         "Operating System :: MacOS",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
