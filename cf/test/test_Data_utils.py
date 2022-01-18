3846d07e8964f54168016f82870bf40f655a6ccf
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2022-01-18 21:25:43 +0000

    Address DH feedback: do not check numpy arrays in test_Data_utils

--- cf/test/test_Data_utils.py
+++ cf/test/test_Data_utils.py
@@ -21,67 +21,52 @@ class DataUtilsTest(unittest.TestCase):
         # and insist on the mask being identical as well as the data
         # (separately, i.e. unmasked) all being 'allclose', so inside our
         # cf.Data objects 'a' and 'a2' would instead *not* be considered equal.
-        a = np.ma.array([1.0, 2.0, 3.0], mask=[1, 0, 0])
-        a2 = np.ma.array([10.0, 2.0, 3.0], mask=[1, 0, 0])
-        b = np.ma.array([1.0, 2.0, 3.0], mask=[0, 1, 0])
-        c = np.ma.array([1.0, 2.0, 100.0], mask=[1, 0, 0])
-        d = np.array([1.0, 2.0, 3.0])
+        a_np = np.ma.array([1.0, 2.0, 3.0], mask=[1, 0, 0])
+        a = da.from_array(a_np)
+        a2 = da.from_array(np.ma.array([10.0, 2.0, 3.0], mask=[1, 0, 0]))
+        b_np = np.ma.array([1.0, 2.0, 3.0], mask=[0, 1, 0])
+        b = da.from_array(b_np)
+        c_np = np.ma.array([1.0, 2.0, 100.0], mask=[1, 0, 0])
+        c = da.from_array(c_np)
+        d = da.from_array(np.array([1.0, 2.0, 3.0]))
         e = a + 5e-04  # outside of tolerance to set, namely rtol=1e-05
         f = a + 5e-06  # within set tolerance to be specified, as above
 
         # Test the function with these inputs as both numpy and dask arrays...
         allclose = cf.data.dask_utils._da_ma_allclose
-        da_ = da.from_array(a)
 
         self.assertTrue(allclose(a, a).compute())
-        self.assertTrue(allclose(da_, da_).compute())
-
         self.assertTrue(allclose(a2, a).compute())
-        self.assertTrue(allclose(da.from_array(a2), da_).compute())
-
         self.assertTrue(allclose(b, a).compute())
-        self.assertTrue(allclose(da.from_array(b), da_).compute())
+
         # ...including testing the 'masked_equal' parameter
         self.assertFalse(allclose(b, a, masked_equal=False).compute())
-        self.assertFalse(
-            allclose(da.from_array(b), da_, masked_equal=False).compute()
-        )
 
         self.assertFalse(allclose(c, a).compute())
-        self.assertFalse(allclose(da.from_array(c), da_).compute())
-
         self.assertTrue(allclose(d, a).compute())
-        self.assertTrue(allclose(da.from_array(d), da_).compute())
-
         self.assertFalse(allclose(e, a).compute())
-        self.assertFalse(allclose(da.from_array(e), da_).compute())
 
         self.assertTrue(allclose(f, a, rtol=1e-05).compute())
-        self.assertTrue(allclose(da.from_array(f), da_, rtol=1e-05).compute())
 
         # Test when array inputs have different chunk sizes
-        da_ = da.from_array(a, chunks=(1, 2))
-        self.assertTrue(allclose(da.from_array(b, chunks=(3,)), da_).compute())
+        a_chunked = da.from_array(a_np, chunks=(1, 2))
+        self.assertTrue(
+            allclose(da.from_array(b_np, chunks=(3,)), a_chunked).compute()
+        )
         self.assertFalse(
             allclose(
-                da.from_array(b, chunks=(3,)), da_, masked_equal=False
+                da.from_array(b_np, chunks=(3,)), a_chunked, masked_equal=False
             ).compute()
         )
         self.assertFalse(
-            allclose(da.from_array(c, chunks=(3,)), da_).compute()
+            allclose(da.from_array(c_np, chunks=(3,)), a_chunked).compute()
         )
 
         # Test the 'rtol' and 'atol' parameters:
         self.assertFalse(allclose(e, a, rtol=1e-06).compute())
-        self.assertFalse(allclose(da.from_array(e), da_, rtol=1e-06).compute())
         b1 = e / 10000
         b2 = a / 10000
         self.assertTrue(allclose(b1, b2, atol=1e-05).compute())
-        self.assertTrue(
-            allclose(
-                da.from_array(b1), da.from_array(b2), atol=1e-05
-            ).compute()
-        )
 
     def test_Data_Utils__is_numeric_dtype(self):
         """TODO."""
