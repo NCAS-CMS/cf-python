dc22fb64789b13550084fb9e019818b295c306b0
David Hassell <david.hassell@ncas.ac.uk>
2022-04-27 10:16:20 +0100

    Data.masked_all

--- cf/test/test_Data.py
+++ cf/test/test_Data.py
@@ -3845,10 +3845,19 @@ class DataTest(unittest.TestCase):
 
         self.assertIsNone(d.override_calendar("all_leap", inplace=True))
 
-    def test_Data_isscalar(self):
-        self.assertTrue(cf.Data(9, "m").isscalar)
-        self.assertFalse(cf.Data([9], "m").isscalar)
-        self.assertFalse(cf.Data([9, 10], "m").isscalar)
+    def test_Data_masked_all(self):
+        # shape
+        for shape in ((), (2,), (2, 3)):
+            a = np.ma.masked_all(shape)
+            d = cf.Data.masked_all(shape)
+            self.assertEqual(d.shape, a.shape)
+            self.assertTrue((d.array.mask == a.mask).all())
+
+        # dtype
+        for dtype in "fibUS":
+            a = np.ma.masked_all((), dtype=dtype)
+            d = cf.Data.masked_all((), dtype=dtype)
+            self.assertEqual(d.dtype, a.dtype)
 
 
 if __name__ == "__main__":
