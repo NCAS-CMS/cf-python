333db1aa76d12303824d5718288c4eb61af7b2fe
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2023-01-27 16:23:17 +0000

    test_aggregate: remove all now-redundant loops over chunksize

--- cf/test/test_aggregate.py
+++ cf/test/test_aggregate.py
@@ -22,158 +22,145 @@ class aggregateTest(unittest.TestCase):
         os.path.dirname(os.path.abspath(__file__)), "file2.nc"
     )
 
-    chunk_sizes = (100000, 300, 34)
-    original_chunksize = cf.chunksize()
-
     def test_basic_aggregate(self):
-        for chunksize in self.chunk_sizes:
-            cf.chunksize(chunksize)
+        f = cf.read(self.filename, squeeze=True)[0]
 
-            f = cf.read(self.filename, squeeze=True)[0]
+        g = cf.FieldList(f[0])
+        g.append(f[1:3])
+        g.append(f[3])
+        g[-1].flip(0, inplace=True)
+        g.append(f[4:7])
+        g[-1].flip(0, inplace=True)
+        g.extend([f[i] for i in range(7, f.shape[0])])
 
-            g = cf.FieldList(f[0])
-            g.append(f[1:3])
-            g.append(f[3])
-            g[-1].flip(0, inplace=True)
-            g.append(f[4:7])
-            g[-1].flip(0, inplace=True)
-            g.extend([f[i] for i in range(7, f.shape[0])])
+        g0 = g.copy()
+        self.assertTrue(g.equals(g0, verbose=-1), "g != g0")
 
-            g0 = g.copy()
-            self.assertTrue(g.equals(g0, verbose=-1), "g != g0")
+        with warnings.catch_warnings():
+            # Suppress noise throughout the test fixture from:
+            #
+            #   ~/cf-python/cf/__init__.py:1459: FutureWarning: elementwise
+            #   comparison failed; returning scalar instead, but in the
+            #   future will perform elementwise comparison
+            #
+            # TODO: it is not clear where the above emerges from, e.g.
+            # since __init__ file ref'd does not have that many lines.
+            # It seems like this warning arises from NumPy comparisons
+            # done at some point in (only) some aggregate calls (see e.g:
+            # https://github.com/numpy/numpy/issues/6784).
+            warnings.filterwarnings("ignore", category=FutureWarning)
+            h = cf.aggregate(g, verbose=2)
 
-            with warnings.catch_warnings():
-                # Suppress noise throughout the test fixture from:
-                #
-                #   ~/cf-python/cf/__init__.py:1459: FutureWarning: elementwise
-                #   comparison failed; returning scalar instead, but in the
-                #   future will perform elementwise comparison
-                #
-                # TODO: it is not clear where the above emerges from, e.g.
-                # since __init__ file ref'd does not have that many lines.
-                # It seems like this warning arises from NumPy comparisons
-                # done at some point in (only) some aggregate calls (see e.g:
-                # https://github.com/numpy/numpy/issues/6784).
-                warnings.filterwarnings("ignore", category=FutureWarning)
-                h = cf.aggregate(g, verbose=2)
+        self.assertEqual(len(h), 1)
 
-            self.assertEqual(len(h), 1)
+        self.assertEqual(
+            h[0].shape,
+            (10, 9),
+            "h[0].shape = " + repr(h[0].shape) + " != (10, 9)",
+        )
 
-            self.assertEqual(
-                h[0].shape,
-                (10, 9),
-                "h[0].shape = " + repr(h[0].shape) + " != (10, 9)",
+        self.assertTrue(
+            g.equals(g0, verbose=2), "g != itself after aggregation"
+        )
+
+        self.assertTrue(h[0].equals(f, verbose=2), "h[0] != f")
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=FutureWarning)
+            i = cf.aggregate(g, verbose=2)
+
+        self.assertTrue(
+            i.equals(h, verbose=2), "The second aggregation != the first"
+        )
+
+        self.assertTrue(
+            g.equals(g0, verbose=2),
+            "g != itself after the second aggregation",
+        )
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=FutureWarning)
+            i = cf.aggregate(g, verbose=2, axes="grid_latitude")
+
+        self.assertTrue(
+            i.equals(h, verbose=2), "The third aggregation != the first"
+        )
+
+        self.assertTrue(
+            g.equals(g0, verbose=2),
+            "g !=itself after the third aggregation",
+        )
+
+        self.assertEqual(
+            i[0].shape, (10, 9), "i[0].shape is " + repr(i[0].shape)
+        )
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=FutureWarning)
+            i = cf.aggregate(
+                g,
+                verbose=2,
+                axes="grid_latitude",
+                donotchecknonaggregatingaxes=1,
             )
 
-            self.assertTrue(
-                g.equals(g0, verbose=2), "g != itself after aggregation"
-            )
+        self.assertTrue(
+            i.equals(h, verbose=2), "The fourth aggregation != the first"
+        )
 
-            self.assertTrue(h[0].equals(f, verbose=2), "h[0] != f")
+        self.assertTrue(
+            g.equals(g0, verbose=2),
+            "g != itself after the fourth aggregation",
+        )
 
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=FutureWarning)
-                i = cf.aggregate(g, verbose=2)
+        self.assertEqual(
+            i[0].shape, (10, 9), "i[0].shape is " + repr(i[0].shape)
+        )
 
-            self.assertTrue(
-                i.equals(h, verbose=2), "The second aggregation != the first"
-            )
+        q, t = cf.read(self.file)
+        c = cf.read(self.file2)[0]
 
-            self.assertTrue(
-                g.equals(g0, verbose=2),
-                "g != itself after the second aggregation",
-            )
+        d = cf.aggregate([c, t], verbose=1, relaxed_identities=True)
+        e = cf.aggregate([t, c], verbose=1, relaxed_identities=True)
 
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=FutureWarning)
-                i = cf.aggregate(g, verbose=2, axes="grid_latitude")
+        self.assertEqual(len(d), 1)
+        self.assertEqual(len(e), 1)
+        self.assertEqual(d[0].shape, (3,) + t.shape)
+        self.assertTrue(d[0].equals(e[0], verbose=2))
 
-            self.assertTrue(
-                i.equals(h, verbose=2), "The third aggregation != the first"
-            )
+        x = cf.read(["file.nc", "file2.nc"], aggregate=False)
+        self.assertEqual(len(x), 3)
 
-            self.assertTrue(
-                g.equals(g0, verbose=2),
-                "g !=itself after the third aggregation",
-            )
+        x = cf.read(
+            ["file.nc", "file2.nc"], aggregate={"relaxed_identities": True}
+        )
+        self.assertEqual(len(x), 2)
 
-            self.assertEqual(
-                i[0].shape, (10, 9), "i[0].shape is " + repr(i[0].shape)
-            )
+        del t.standard_name
+        del c.standard_name
+        x = cf.aggregate([c, t], verbose=1)
+        self.assertEqual(len(x), 2)
 
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=FutureWarning)
-                i = cf.aggregate(
-                    g,
-                    verbose=2,
-                    axes="grid_latitude",
-                    donotchecknonaggregatingaxes=1,
-                )
-
-            self.assertTrue(
-                i.equals(h, verbose=2), "The fourth aggregation != the first"
-            )
-
-            self.assertTrue(
-                g.equals(g0, verbose=2),
-                "g != itself after the fourth aggregation",
-            )
-
-            self.assertEqual(
-                i[0].shape, (10, 9), "i[0].shape is " + repr(i[0].shape)
-            )
-
-            q, t = cf.read(self.file)
-            c = cf.read(self.file2)[0]
-
-            d = cf.aggregate([c, t], verbose=1, relaxed_identities=True)
-            e = cf.aggregate([t, c], verbose=1, relaxed_identities=True)
-
-            self.assertEqual(len(d), 1)
-            self.assertEqual(len(e), 1)
-            self.assertEqual(d[0].shape, (3,) + t.shape)
-            self.assertTrue(d[0].equals(e[0], verbose=2))
-
-            x = cf.read(["file.nc", "file2.nc"], aggregate=False)
-            self.assertEqual(len(x), 3)
-
-            x = cf.read(
-                ["file.nc", "file2.nc"], aggregate={"relaxed_identities": True}
-            )
-            self.assertEqual(len(x), 2)
-
-            del t.standard_name
-            del c.standard_name
-            x = cf.aggregate([c, t], verbose=1)
-            self.assertEqual(len(x), 2)
-
-            t.long_name = "qwerty"
-            c.long_name = "qwerty"
-            x = cf.aggregate([c, t], field_identity="long_name")
-            self.assertEqual(len(x), 1)
-
-        cf.chunksize(self.original_chunksize)
+        t.long_name = "qwerty"
+        c.long_name = "qwerty"
+        x = cf.aggregate([c, t], field_identity="long_name")
+        self.assertEqual(len(x), 1)
 
     def test_aggregate_exist_equal_ignore_opts(self):
         # TODO: extend the option-checking coverage so all options and all
         # reasonable combinations of them are tested. For now, this is
         # testing options that previously errored due to a bug.
-        for chunksize in self.chunk_sizes:
-            cf.chunksize(chunksize)
+        f = cf.read(self.filename, squeeze=True)[0]
 
-            f = cf.read(self.filename, squeeze=True)[0]
+        # Use f as-is: simple test that aggregate works and does not
+        # change anything with the given options:
+        g = cf.aggregate(f, exist_all=True)[0]
+        self.assertEqual(g, f)
+        h = cf.aggregate(f, equal_all=True)[0]
+        self.assertEqual(h, f)
 
-            # Use f as-is: simple test that aggregate works and does not
-            # change anything with the given options:
-            g = cf.aggregate(f, exist_all=True)[0]
-            self.assertEqual(g, f)
-            h = cf.aggregate(f, equal_all=True)[0]
-            self.assertEqual(h, f)
-
-            with self.assertRaises(ValueError):  # contradictory options
-                cf.aggregate(f, exist_all=True, equal_all=True)
-
-        cf.chunksize(self.original_chunksize)
+        with self.assertRaises(ValueError):  # contradictory options
+            cf.aggregate(f, exist_all=True, equal_all=True)
 
     def test_aggregate_verbosity(self):
         f0 = cf.example_field(0)
