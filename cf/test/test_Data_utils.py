d698e7deeace1cc2a681e5a52baaf400d60c61fd
David Hassell <david.hassell@ncas.ac.uk>
2023-01-30 14:44:05 +0000

    fix is_numeric_dtype name

--- cf/test/test_Data_utils.py
+++ cf/test/test_Data_utils.py
@@ -69,22 +69,22 @@ class DataUtilsTest(unittest.TestCase):
         b2 = a / 10000
         self.assertTrue(allclose(b1, b2, atol=1e-05).compute())
 
-    def test_Data_Utils__is_numeric_dtype(self):
+    def test_Data_Utils_is_numeric_dtype(self):
         """TODO."""
-        _is_numeric_dtype = cf.data.utils._is_numeric_dtype
+        is_numeric_dtype = cf.data.utils.is_numeric_dtype
         for a in [
             np.array([0, 1, 2]),
             np.array([False, True, True]),
             np.ma.array([10.0, 2.0, 3.0], mask=[1, 0, 0]),
             np.array(10),
         ]:
-            self.assertTrue(_is_numeric_dtype(a))
+            self.assertTrue(is_numeric_dtype(a))
 
         for b in [
             np.array(["a", "b", "c"], dtype="S1"),
             np.empty(1, dtype=object),
         ]:
-            self.assertFalse(_is_numeric_dtype(b))
+            self.assertFalse(is_numeric_dtype(b))
 
     def test_Data_Utils_convert_to_datetime(self):
         """TODO."""
