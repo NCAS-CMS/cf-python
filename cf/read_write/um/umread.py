correct bounds and units of PP hybrid height coordinates

cf/read_write/um/umread.py
@@ -38,7 +38,7 @@ _cached_latlon = {}
 _cached_time = {}
 _cached_ctime = {}
 _cached_size_1_height_coordinate = {}
-_cached_z_reference_coordinate = {}
+# _cached_z_reference_coordinate = {}
 _cached_date2num = {}
 _cached_model_level_number_coordinate = {}
 
@@ -1130,10 +1130,11 @@ class UMField:
     def _reorder_z_axis(self, indices, z_axis, pmaxes):
         """Reorder the Z axis `Rec` instances.
 
-        :Parmaeters:
+        :Parameters:
 
             indices: `list`
-                Aggregation axis indices. See `create_data` for details.
+                Aggregation axis indices. See `create_data` for
+                details.
 
             z_axis: `int`
                 The identifier of the Z axis
@@ -1141,17 +1142,23 @@ class UMField:
             pmaxes: sequence of `int`
                 The aggregation axes, which include the Z axis.
 
+        :Returns:
+
+            `list`
+
         **Examples**
 
         >>> _reorder_z_axis([(0, <Rec A>), (1, <Rec B>)], 0, [0])
         [(0, <Rec B>), (1, <Rec A>)]
 
         >>> _reorder_z_axis(
-        ...     [(0, 0, <Rec A>), (0, 1, <Rec B>), (1, 0, <Rec C>), (1, 1, <Rec D>)],
+        ...     [(0, 0, <Rec A>),
+        ...      (0, 1, <Rec B>),
+        ...      (1, 0, <Rec C>),
+        ...      (1, 1, <Rec D>)],
         ...     1, [0, 1]
         ... )
         [(0, 0, <Rec B>), (0, 1, <Rec A>), (1, 0, <Rec D>), (1, 1, <Rec C>)]
-
 
         """
         indices_new = []
@@ -1282,8 +1289,9 @@ class UMField:
             dc = None
         else:
             array = array / toa_height
+            bounds = bounds / toa_height
             dc = self.implementation.initialise_DimensionCoordinate()
-            dc = self.coord_data(dc, array, bounds, units=_Units[""])
+            dc = self.coord_data(dc, array, bounds, units=_Units["1"])
             self.implementation.set_properties(
                 dc,
                 {"standard_name": "atmosphere_hybrid_height_coordinate"},
@@ -3215,70 +3223,71 @@ class UMField:
 
         return dc
 
-    @_manage_log_level_via_verbose_attr
-    def z_reference_coordinate(self, axiscode):
-        """Create and return the Z reference coordinates."""
-        logger.info(
-            "Creating Z reference coordinates from BRLEV"
-        )  # pragma: no cover
 
-        array = np.array(
-            [rec.real_hdr.item(brlev) for rec in self.z_recs], dtype=float
-        )
-
-        LBVC = self.lbvc
-        atol = self.atol
-
-        key = (axiscode, LBVC, array)
-        dc = _cached_z_reference_coordinate.get(key, None)
-
-        if dc is not None:
-            copy = True
-        else:
-            if not 128 <= LBVC <= 139:
-                bounds = []
-                for rec in self.z_recs:
-                    BRLEV = rec.real_hdr.item(brlev)
-                    BRSVD1 = rec.real_hdr.item(brsvd1)
-
-                    if abs(BRSVD1 - BRLEV) >= atol:
-                        bounds = None
-                        break
-
-                    bounds.append((BRLEV, BRSVD1))
-            else:
-                bounds = None
-
-            if bounds:
-                bounds = np.array((bounds,), dtype=float)
-
-            dc = self.implementation.initialise_DimensionCoordinate()
-            dc = self.coord_data(
-                dc,
-                array,
-                bounds,
-                units=_axiscode_to_Units.setdefault(axiscode, None),
-            )
-            dc = self.coord_axis(dc, axiscode)
-            dc = self.coord_names(dc, axiscode)
-
-            if not dc.get("positive", True):  # ppp
-                dc.flip(inplace=True)
-
-            _cached_z_reference_coordinate[key] = dc
-            copy = False
-
-        self.implementation.set_dimension_coordinate(
-            self.field,
-            dc,
-            axes=[_axis["z"]],
-            copy=copy,
-            autocyclic=_autocyclic_false,
-        )
-
-        return dc
-
-
+#    @_manage_log_level_via_verbose_attr
+#    def z_reference_coordinate(self, axiscode):
+#        """Create and return the Z reference coordinates."""
+#        logger.info(
+#            "Creating Z reference coordinates from BRLEV"
+#        )  # pragma: no cover
+#
+#        array = np.array(
+#            [rec.real_hdr.item(brlev) for rec in self.z_recs], dtype=float
+#        )
+#
+#        LBVC = self.lbvc
+#        atol = self.atol
+#        print(99999)
+#        key = (axiscode, LBVC, array)
+#        dc = _cached_z_reference_coordinate.get(key)
+#
+#        if dc is not None:
+#            copy = True
+#        else:
+#            if not 128 <= LBVC <= 139:
+#                bounds = []
+#                for rec in self.z_recs:
+#                    BRLEV = rec.real_hdr.item(brlev)
+#                    BRSVD1 = rec.real_hdr.item(brsvd1)
+#
+#                    if abs(BRSVD1 - BRLEV) >= atol:
+#                        bounds = None
+#                        break
+#
+#                    bounds.append((BRLEV, BRSVD1))
+#            else:
+#                bounds = None
+#
+#            if bounds:
+#                bounds = np.array((bounds,), dtype=float)
+#
+#            dc = self.implementation.initialise_DimensionCoordinate()
+#            dc = self.coord_data(
+#                dc,
+#                array,
+#                bounds,
+#                units=_axiscode_to_Units.setdefault(axiscode, None),
+#            )
+#            dc = self.coord_axis(dc, axiscode)
+#            dc = self.coord_names(dc, axiscode)
+#
+#            if not dc.get("positive", True):  # ppp
+#                dc.flip(inplace=True)
+#
+#            _cached_z_reference_coordinate[key] = dc
+#            copy = False
+#
+#        self.implementation.set_dimension_coordinate(
+#            self.field,
+#            dc,
+#            axes=[_axis["z"]],
+#            copy=copy,
+#            autocyclic=_autocyclic_false,
+#        )
+#
+#        return dc
+#
+#
 # _stash2standard_name = {}
 #
 # def load_stash2standard_name(table=None, delimiter='!', merge=True):