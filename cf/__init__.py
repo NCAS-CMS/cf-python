4b729c4885f007a0fcb7f1a18de35e51cb87c09a
David Hassell <david.hassell@ncas.ac.uk>
2023-06-09 12:32:40 +0100

    version/date

--- cf/__init__.py
+++ cf/__init__.py
@@ -74,8 +74,8 @@ installation and source code.
 """
 
 __Conventions__ = "CF-1.10"
-__date__ = "2023-04-27"
-__version__ = "3.15.0"
+__date__ = "2023-06-09"
+__version__ = "3.15.1"
 
 _requires = (
     "numpy",
