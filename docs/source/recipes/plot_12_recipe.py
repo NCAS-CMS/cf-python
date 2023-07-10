f879b6b9cf9e6317403d66b60a682d9a0fbf1475
bewithankit <bhandekar.ankit@outlook.com>
2023-07-10 15:28:13 +0100

    Update files

--- docs/source/recipes/plot_12_recipe.py
+++ docs/source/recipes/plot_12_recipe.py
@@ -45,19 +45,19 @@ lat = aod.coordinate("Y")
 # 6. Plot the AOD for all the retrievals using
 # `cfplot.con <http://ajheaps.github.io/cf-plot/con.html>`_. Here the argument
 # ``'ptype'`` specifies the type of plot to use (latituide-longitude here) and
-# the argument ``'lines=False'``does not draw contour lines:
+# the argument ``'lines=False'`` does not draw contour lines:
 cfp.con(f=aod.array, x=lon.array, y=lat.array, ptype=1, lines=False)
 
 # %%
 # 7. Create a mask for AOD based on the quality of the retrieval. The
-# ``'__ne__'`` method is an implementation of the `!=` operator. It is used to
+# ``'__ne__'`` method is an implementation of the ``!=`` operator. It is used to
 # create a mask where all the `high quality` AOD points (with the flag 0) are
 # marked as ``False``, and all the other data points (medium quality, low
 # quality, or no retrieval) are marked as ``True``:
 mask = quality.array.__ne__(0)
 
 # %%
-# 8. Now apply the mask to the AOD dataset. The ``'where'`` function takes the
+# 8. Apply the mask to the AOD dataset. The ``'where'`` function takes the
 # mask as an input and replaces all the values in the AOD dataset that
 # correspond to ``True`` in the mask with a masked value using `cf.masked
 # <https://ncas-cms.github.io/cf-python/constant.html#cf.cf.masked>`_.
@@ -67,7 +67,22 @@ mask = quality.array.__ne__(0)
 high = aod.where(mask, cf.masked)
 
 # %%
-# 9. Now
+# 9. Now plot both the AOD from high quality retrieval and all other retrievals 
+# using `cfplot.con <http://ajheaps.github.io/cf-plot/con.html>`_. Here 
+# `cfplot.gopen <http://ajheaps.github.io/cf-plot/gopen.html>`_ is used to 
+# define the parts of the plot area, specifying that the figure should have 
+# 1 row and 2 columns which is closed by 
+# `cfplot.gclose <http://ajheaps.github.io/cf-plot/gclose.html>`_;
+# `plt.suptitle <https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.suptitle.html>`_
+# is used to add a title for the whole figure; 
+# the subplots for plotting are selected using 
+# `cfplot.gpos <https://ajheaps.github.io/cf-plot/gpos.html>`_ after which 
+# `cfplot.mapset <https://ajheaps.github.io/cf-plot/mapset.html>`_ is used to 
+# set the map limits and resolution for the subplots;  
+# as cf-plot stores the plot in a plot object with the name 
+# ``cfp.plotvars.plot``, country borders are added using normal 
+# `Cartopy operations <https://scitools.org.uk/cartopy/docs/latest/reference/index.html>`_ 
+# on the ``cfp.plotvars.mymap`` object:
 import cartopy.feature as cfeature
 
 cfp.gopen(rows=1, columns=2, bottom=0.2)
