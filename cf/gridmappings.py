638b6f0835eccf4d6f8553b51757d4673774aa52
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2023-07-18 15:00:42 +0100

    Inheritance from abstract to concrete Grid Mapping classes

--- cf/gridmappings.py
+++ cf/gridmappings.py
@@ -3,23 +3,23 @@ from pyproj import CRS
 ALL_GRID_MAPPING_ATTR_NAMES = {
     "grid_mapping_name",
     # *Those which describe the ellipsoid and prime meridian:*
-    "earth_radius",
-    "inverse_flattening",
+    "earth_radius",  # PROJ +R
+    "inverse_flattening",  # PROJ "+rf"
     "longitude_of_prime_meridian",
-    "prime_meridian_name",
-    "reference_ellipsoid_name",
-    "semi_major_axis",
-    "semi_minor_axis",
+    "prime_meridian_name",  # PROJ +pm
+    "reference_ellipsoid_name",  # PROJ +ellps
+    "semi_major_axis",  # PROJ "+a"
+    "semi_minor_axis",  # PROJ "+b"
     # *Specific/applicable to only given grid mapping(s):*
     # ...projection origin related:
-    "longitude_of_projection_origin",
-    "latitude_of_projection_origin",
-    "scale_factor_at_projection_origin",
+    "longitude_of_projection_origin",  # PROJ +lon_0
+    "latitude_of_projection_origin",  # PROJ +lat_0
+    "scale_factor_at_projection_origin",  # PROJ k_0
     # ...false-Xings:
-    "false_easting",
-    "false_northing",
+    "false_easting",  # PROJ +x_0
+    "false_northing",  # PROJ +y_0
     # ...angle axis related:
-    "sweep_angle_axis",
+    "sweep_angle_axis",  # PROJ +sweep
     "fixed_angle_axis",
     # ...central meridian related:
     "longitude_of_central_meridian",
@@ -29,21 +29,22 @@ ALL_GRID_MAPPING_ATTR_NAMES = {
     "grid_north_pole_longitude",
     "north_pole_grid_longitude",
     # ...other:
-    "standard_parallel",
-    "perspective_point_height",
-    "azimuth_of_central_line",
-    "straight_vertical_longitude_from_pole",
+    "standard_parallel",  # PROJ ["+lat_1", "+lat_2"] (up to 2)
+    "perspective_point_height",  # PROJ "+h"
+    "azimuth_of_central_line",  # PROJ +gamma OR +alpha
+    "straight_vertical_longitude_from_pole",  # PROJ +south
     # *Other, not needed for a specific grid mapping but also listed
     # in 'Table F.1. Grid Mapping Attributes':*
-    "crs_wkt",
-    "geographic_crs_name",
-    "geoid_name",
+    "crs_wkt" "geographic_crs_name",  # PROJ "crs_wkt",  # PROJ geoid_crs
+    "geoid_name",  # PROJ geoidgrids
     "geopotential_datum_name",
     "horizontal_datum_name",
-    "inverse_flattening",
     "projected_crs_name",
-    "towgs84",
+    "towgs84",  # PROJ +towgs84
 }
+
+
+"""Abstract classes for general Grid Mappings."""
 
 
 class GridMapping:
@@ -69,7 +70,7 @@ class GridMapping:
                 TODO
 
             proj_id: string
-                TODO
+                TODO "_proj" projection name
 
             earth_radius: number, optional
                 TODO
@@ -101,62 +102,109 @@ class GridMapping:
                 "base PROJ '+proj' identifier with the proj_id parameter."
             )
 
+        # Defining the Grid Mapping
+        self.grid_mapping_name = grid_mapping_name
+        self.proj_id = proj_id
+
+        # The attributes which describe the ellipsoid and prime meridian,
+        # which may be included, when applicable, with any grid mapping
+        self.earth_radius = earth_radius
+        self.inverse_flattening = inverse_flattening
+        self.longitude_of_prime_meridian = longitude_of_prime_meridian
+        self.prime_meridian_name = prime_meridian_name
+        self.reference_ellipsoid_name = reference_ellipsoid_name
+        self.semi_major_axis = semi_major_axis
+        self.semi_minor_axis = semi_minor_axis
+
 
 class AzimuthalGridMapping(GridMapping):
-    """TODO."""
+    """A Grid Mapping with Azimuthal classification."""
 
     def __init__(
         self,
         longitude_of_projection_origin,
         latitude_of_projection_origin,
+        false_easting,
+        false_northing,
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
 
+        self.longitude_of_projection_origin = longitude_of_projection_origin
+        self.latitude_of_projection_origin = latitude_of_projection_origin
+        self.false_easting = false_easting
+        self.false_northing = false_northing
+
 
 class ConicGridMapping(GridMapping):
-    """TODO."""
+    """A Grid Mapping with Conic classification."""
 
     def __init__(
-        self, standard_parallel, longitude_of_central_meridian, *args, **kwargs
+        self,
+        standard_parallel,
+        longitude_of_central_meridian,
+        latitude_of_projection_origin,
+        false_easting,
+        false_northing,
+        *args,
+        **kwargs,
     ):
         super().__init__(*args, **kwargs)
 
+        self.standard_parallel = standard_parallel
+        self.longitude_of_central_meridian = longitude_of_central_meridian
+        self.latitude_of_projection_origin = latitude_of_projection_origin
+        self.false_easting = false_easting
+        self.false_northing = false_northing
+
 
 class CylindricalGridMapping(GridMapping):
-    """TODO."""
+    """A Grid Mapping with Cylindrical classification."""
 
     def __init__(self, false_easting, false_northing, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.false_easting = false_easting
+        self.false_northing = false_northing
+
 
 class LatLonGridMapping(GridMapping):
-    """TODO."""
+    """A Grid Mapping with Latitude-Longitude nature.
+
+    Such a Grid Mapping is based upon latitude and longitude coordinates
+    on a spherical Earth, defining the canonical 2D geographical coordinate
+    system so that the figure of the Earth can be described.
+
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class PerspectiveGridMapping(AzimuthalGridMapping):
-    """TODO."""
+    """A Grid Mapping with Azimuthal classification and perspective view."""
 
     def __init__(
         self,
-        false_easting,
-        false_northing,
         perspective_point_height,
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
 
+        self.perspective_point_height = perspective_point_height
 
-class StereographicGridMapping(AzimuthalGridMapping):
-    """TODO."""
 
-    def __init__(self, false_easting, false_northing, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+"""Concrete classes for all Grid Mappings supported by the CF Conventions.
+
+For the full listing, see:
+
+https://cfconventions.org/Data/cf-conventions/cf-conventions-1.10/
+cf-conventions.html#appendix-grid-mappings
+
+from which these classes should be kept consistent and up-to-date.
+"""
 
 
 class AlbersEqualArea(ConicGridMapping):
@@ -180,7 +228,7 @@ class AlbersEqualArea(ConicGridMapping):
         self,
         standard_parallel,
         longitude_of_central_meridian,
-        atitude_of_projection_origin,
+        latitude_of_projection_origin,
         false_easting,
         false_northing,
         *args,
@@ -237,17 +285,20 @@ class Geostationary(PerspectiveGridMapping):
 
     def __init__(
         self,
-        latitude_of_projection_origin,
         longitude_of_projection_origin,
-        perspective_point_height,
+        latitude_of_projection_origin,
         false_easting,
         false_northing,
+        perspective_point_height,
         sweep_angle_axis,
         fixed_angle_axis,
         *args,
         **kwargs,
     ):
         super().__init__("geostationary", "geos", *args, **kwargs)
+
+        self.sweep_angle_axis = sweep_angle_axis
+        self.fixed_angle_axis = fixed_angle_axis
 
 
 class LambertAzimuthalEqualArea(AzimuthalGridMapping):
@@ -330,16 +381,22 @@ class LambertCylindricalEqualArea(CylindricalGridMapping):
 
     def __init__(
         self,
-        longitude_of_central_meridian,
-        standard_parallel,
-        scale_factor_at_projection_origin,
         false_easting,
         false_northing,
+        standard_parallel,
+        longitude_of_central_meridian,
+        scale_factor_at_projection_origin,
         *args,
         **kwargs,
     ):
         super().__init__(
             "lambert_cylindrical_equal_area", "cea", *args, **kwargs
+        )
+
+        self.standard_parallel = standard_parallel
+        self.longitude_of_central_meridian = longitude_of_central_meridian
+        self.scale_factor_at_projection_origin = (
+            scale_factor_at_projection_origin
         )
 
 
@@ -362,15 +419,21 @@ class Mercator(CylindricalGridMapping):
 
     def __init__(
         self,
-        longitude_of_projection_origin,
-        standard_parallel,
-        scale_factor_at_projection_origin,
         false_easting,
         false_northing,
+        standard_parallel,
+        longitude_of_projection_origin,
+        scale_factor_at_projection_origin,
         *args,
         **kwargs,
     ):
         super().__init__("mercator", "merc", *args, **kwargs)
+
+        self.standard_parallel = standard_parallel
+        self.longitude_of_projection_origin = longitude_of_projection_origin
+        self.scale_factor_at_projection_origin = (
+            scale_factor_at_projection_origin
+        )
 
 
 class ObliqueMercator(CylindricalGridMapping):
@@ -392,16 +455,23 @@ class ObliqueMercator(CylindricalGridMapping):
 
     def __init__(
         self,
+        false_easting,
+        false_northing,
         azimuth_of_central_line,
         latitude_of_projection_origin,
         longitude_of_projection_origin,
         scale_factor_at_projection_origin,
-        false_easting,
-        false_northing,
         *args,
         **kwargs,
     ):
         super().__init__("oblique_mercator", "omerc", *args, **kwargs)
+
+        self.azimuth_of_central_line = azimuth_of_central_line
+        self.latitude_of_projection_origin = latitude_of_projection_origin
+        self.longitude_of_projection_origin = longitude_of_projection_origin
+        self.scale_factor_at_projection_origin = (
+            scale_factor_at_projection_origin
+        )
 
 
 class Orthographic(AzimuthalGridMapping):
@@ -433,7 +503,7 @@ class Orthographic(AzimuthalGridMapping):
         super().__init__("orthographic", "ortho", *args, **kwargs)
 
 
-class PolarStereographic(StereographicGridMapping):
+class PolarStereographic(AzimuthalGridMapping):
     """The Universal Polar Stereographic grid mapping.
 
     See the CF Conventions document 'Appendix F: Grid Mappings' sub-section on
@@ -452,16 +522,35 @@ class PolarStereographic(StereographicGridMapping):
 
     def __init__(
         self,
-        straight_vertical_longitude_from_pole,
         latitude_of_projection_origin,
-        standard_parallel,
-        scale_factor_at_projection_origin,
         false_easting,
         false_northing,
+        standard_parallel,
+        scale_factor_at_projection_origin,
+        longitude_of_projection_origin=None,
+        straight_vertical_longitude_from_pole=None,
         *args,
         **kwargs,
     ):
         super().__init__("polar_stereographic", "ups", *args, **kwargs)
+
+        # See: https://github.com/cf-convention/cf-conventions/issues/445
+        if (
+            longitude_of_projection_origin
+            and straight_vertical_longitude_from_pole
+        ):
+            raise ValueError(
+                "Only one of 'longitude_of_projection_origin' and "
+                "'straight_vertical_longitude_from_pole' can be set."
+            )
+
+        self.straight_vertical_longitude_from_pole = (
+            straight_vertical_longitude_from_pole
+        )
+        self.standard_parallel = standard_parallel
+        self.scale_factor_at_projection_origin = (
+            scale_factor_at_projection_origin
+        )
 
 
 class RotatedLatitudeLongitude(LatLonGridMapping):
@@ -491,8 +580,12 @@ class RotatedLatitudeLongitude(LatLonGridMapping):
     ):
         super().__init__("rotated_latitude_longitude", "eqc", *args, **kwargs)
 
+        self.grid_north_pole_latitude = grid_north_pole_latitude
+        self.grid_north_pole_longitude = grid_north_pole_longitude
+        self.north_pole_grid_longitude = north_pole_grid_longitude
 
-class LatitudeLongitude(RotatedLatitudeLongitude):
+
+class LatitudeLongitude(LatLonGridMapping):
     """The Latitude-Longitude i.e. Plate Carrée grid mapping.
 
     For alternative names, see e.g:
@@ -544,8 +637,12 @@ class Sinusoidal(GridMapping):
     ):
         super().__init__("sinusoidal", "sinu", *args, **kwargs)
 
+        self.longitude_of_projection_origin = longitude_of_projection_origin
+        self.false_easting = false_easting
+        self.false_northing = false_northing
 
-class Stereographic(StereographicGridMapping):
+
+class Stereographic(AzimuthalGridMapping):
     """The Stereographic grid mapping.
 
     See the CF Conventions document 'Appendix F: Grid Mappings' sub-section on
@@ -566,13 +663,17 @@ class Stereographic(StereographicGridMapping):
         self,
         longitude_of_projection_origin,
         latitude_of_projection_origin,
-        scale_factor_at_projection_origin,
         false_easting,
         false_northing,
+        scale_factor_at_projection_origin,
         *args,
         **kwargs,
     ):
         super().__init__("stereographic", "stere", *args, **kwargs)
+
+        self.scale_factor_at_projection_origin = (
+            scale_factor_at_projection_origin
+        )
 
 
 class TransverseMercator(CylindricalGridMapping):
@@ -594,15 +695,21 @@ class TransverseMercator(CylindricalGridMapping):
 
     def __init__(
         self,
+        false_easting,
+        false_northing,
         scale_factor_at_central_meridian,
         longitude_of_central_meridian,
         latitude_of_projection_origin,
-        false_easting,
-        false_northing,
         *args,
         **kwargs,
     ):
         super().__init__("transverse_mercator", "tmerc", *args, **kwargs)
+
+        self.scale_factor_at_central_meridian = (
+            scale_factor_at_central_meridian
+        )
+        self.longitude_of_central_meridian = longitude_of_central_meridian
+        self.latitude_of_projection_origin = latitude_of_projection_origin
 
 
 class VerticalPerspective(PerspectiveGridMapping):
@@ -624,11 +731,11 @@ class VerticalPerspective(PerspectiveGridMapping):
 
     def __init__(
         self,
-        latitude_of_projection_origin,
         longitude_of_projection_origin,
-        perspective_point_height,
+        latitude_of_projection_origin,
         false_easting,
         false_northing,
+        perspective_point_height,
         *args,
         **kwargs,
     ):
