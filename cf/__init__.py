bdc622560ff0aaf53f4f806675d621b22878594a
David Hassell <david.hassell@ncas.ac.uk>
2024-03-01 13:02:50 +0000

    new archive docs script

--- cf/__init__.py
+++ cf/__init__.py
@@ -199,8 +199,8 @@ if Version(cfunits.__version__) < Version(_minimum_vn):
     )
 
 # Check the version of cfdm
-_minimum_vn = "1.11.0.0"
-_maximum_vn = "1.11.1.0"
+_minimum_vn = "1.11.1.0"
+_maximum_vn = "1.11.2.0"
 _cfdm_version = Version(cfdm.__version__)
 if not Version(_minimum_vn) <= _cfdm_version < Version(_maximum_vn):
     raise RuntimeError(
