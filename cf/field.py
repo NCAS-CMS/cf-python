f696627d38a5315127ba5056e78395a8694b25cd
David Hassell <davidhassell@users.noreply.github.com>
2024-03-23 18:50:56 +0000

    Replace loop with lisyt comprehension
    Co-authored-by: Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>

--- cf/field.py
+++ cf/field.py
@@ -454,9 +454,8 @@ class Field(mixin.FieldDomain, mixin.PropertiesData, cfdm.Field):
         # below.
         if org_cyclic:
             new_cyclic = new_data.cyclic()
-            for i in org_cyclic:
-                if i not in new_cyclic:
-                    new.cyclic(i, iscyclic=False)
+            [new.cyclic(i, iscyclic=False) for i in org_cyclic if i not in new_cyclic]
+                    
 
         # ------------------------------------------------------------
         # Subspace constructs with data
