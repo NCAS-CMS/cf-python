67d253b661b0a9ea7f69c93ea726c49266740aa6
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-10-03 15:06:27 +0100

    Simplify code given revised proj choices in NH original recipe 16

--- docs/source/recipes/plot_16_recipe.py
+++ docs/source/recipes/plot_16_recipe.py
@@ -17,47 +17,32 @@ import cf
 
 # %%
 # 2. Read the field in:
-# Here I've used sample data ggap.nc (and later pressure=850),
-# but you could use tas_A1.nc (with time=15)
 PATH = "~/git-repos/cf-plot/cfplot/test/cfplot_data"
 f = cf.read(f"{PATH}/ggap.nc")[0]
 
 # %%
-# 3. Create the file with subplots:
-# If you are changing the number of subplots ensure the number of
-# rows * number of columns = the number of subplots/projections
-# Here we are doing 6 projections so 2x3 is fine
+# 3. Create the file with subplots. If changing the number of subplots,
+# ensure the number of rows * number of columns = the number of projections.
+# Here we are doing 6 projections so 2 x 3 is fine:
 cfp.gopen(rows=2, columns=3, bottom=0.2, file="projections.png")
 
 # %%
-# 4. List the projection types being used:
-# Here we are using Cylindrical/Default, North Pole Stereographic,
-# South Pole Stereographic, Mollweide, Cropped Lambert Conformal
-# and Robinson.
-# However you could also use other such as "rotated", "ortho" or
-# "merc", "ukcp", "osgb", or "EuroPP"
-# https://ncas-cms.github.io/cf-plot/build/user_guide.html#appendixc
-# TODO SB update second 'cyl' to 'lcc', replaced for now due to possible bug,
-# see: https://github.com/NCAS-CMS/cf-plot/issues/75
-projtypes = ["cyl", "npstere", "spstere", "moll", "cyl", "robin"]
+# 4. List the projection types to use. Here we are using
+# Cylindrical/Default, North Pole Stereographic, South Pole Stereographic,
+# Mollweide, Mercator and Robinson. However there are several other choices
+# possible, see:
+# https://ncas-cms.github.io/cf-plot/build/user_guide.html#appendixc. Our
+# chosen list is:
+projtypes = ["cyl", "npstere", "spstere", "moll", "merc", "robin"]
 
 # %%
-# 5. We then use a for loop to cycle through all the different projection
-# types:
-# Only gpos has 1 added because it can only take 1 as its first value,
-# otherwise there are
-# errors. There are if statements for some projections (lcc, OSGB and
-# EuroPP) as they have
-# specific requirements for their contour.
-# However, OSGB and EuroPP will require very different data anyway.
+# 5. Loop through the list of projection types and plot each as a sub-plot:
 for i, proj in enumerate(projtypes):
+    # gpos has 1 added to the index because it takes 1 as its first value
     cfp.gpos(i + 1)
-    if proj == "lcc":
-        cfp.mapset(proj="lcc", lonmin=-50, lonmax=50, latmin=20, latmax=85)
-    if proj in ("OSGB", "EuroPP"):
-        cfp.mapset(proj=proj, resolution="50m")
-    else:
-        cfp.mapset(proj=proj)
+    cfp.mapset(proj=proj)
+
+    # For the final plot only, add a colour bar to cover all the sub-plots
     if i == len(projtypes) - 1:
         cfp.con(
             f.subspace(pressure=850),
@@ -66,8 +51,6 @@ for i, proj in enumerate(projtypes):
             colorbar_position=[0.1, 0.1, 0.8, 0.02],
             colorbar_orientation="horizontal",
         )
-        # to see the marking lines need to be True, but this can be
-        # hard to read so if not needed use False
     else:
         cfp.con(
             f.subspace(pressure=850),
