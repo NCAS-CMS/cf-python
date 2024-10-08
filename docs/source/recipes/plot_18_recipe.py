727cb451d60529660be69788c8b231a7c604b7ef
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-10-08 17:35:11 +0100

    Update NH original recipe 18 to produce one plot only w/ result

--- docs/source/recipes/plot_18_recipe.py
+++ docs/source/recipes/plot_18_recipe.py
@@ -12,6 +12,7 @@ elevation line plot.
 
 import cfplot as cfp
 import cf
+
 import sys
 import scipy.stats.mstats as mstats
 import matplotlib.pyplot as plt
@@ -25,11 +26,11 @@ import matplotlib.pyplot as plt
 PATH="~/summerstudents/final-recipes/new-required-datasets"
 orog = cf.read(f"{PATH}/1km_elevation.nc")[0]
 snow = cf.read(f"{PATH}/snowcover")[0]
-
+# Could use any of the seven days by indexing differently
 snow_day = snow[0]  # first day, Jan 1st of this year (2024)
 
 #%%
-# 3. Choose the regions:
+# 3. Choose the regions and subspace to same area for both datasets:
 # Ensuring both datasets cover it, and also that its not too large as then 
 # regridding could cause the program to crash on small computers (dont 
 # regrid the whole map), as well as being mainly land.
@@ -40,104 +41,61 @@ use_orog = orog.subspace(
     longitude=cf.wi(*region_in_mid_uk[0]),
     latitude=cf.wi(*region_in_mid_uk[1])
 )
-
-#%%
-# 4. Subspace snow to same bounding box as orography data (orog)
 use_snow = snow_day.subspace(
     longitude=cf.wi(*region_in_mid_uk[0]),
     latitude=cf.wi(*region_in_mid_uk[1])
 )
 
 #%%
-# 5. Normalise the data
-# This normalises to between 0 and 1 so multiply by 100 to get a percentage
-use_snow_normal = ((use_snow - use_snow.minimum())/ (use_snow.range()))*100 
-
-#%%
-# 6. Reassign the units as they are removed by cf-python after calculation
+# 4. Normalise the data and change the units appropriately.
+# Reassign the units as they are removed by cf-python after calculation
 # Only do this if you are certain the units are convertible to %
-use_snow_normal.override_units("percentage", inplace=True) 
+# This normalises to between 0 and 1 so multiply by 100 to get a percentage:
+use_snow_normal = ((use_snow - use_snow.minimum())/ (use_snow.range()))*100 
+# TODO SB not CF Compliant way to handle normalisation, check
+use_snow_normal.override_units("percentage", inplace=True)
 
 #%%
-# 7. Plot of Snowcover contour
+# 5. Regrid the data to have a comparable array.
+# Here the orography file is regridded to the snow file 
+# as snow is higher resolution (if this doesnt work switch them).
+reg = use_orog.regrids(use_snow_normal, method="linear")
+
+#%%
+# 6. Squeeze snow data to remove the size 1 axes
+use_snow_normal = use_snow_normal.squeeze()
+
+#%%
+#  7. Final statistical calculations
+coefficient = mstats.pearsonr(reg.array, use_snow_normal.array)
+print(f"The Pearson correlation coefficient is {coefficient}")
+
+
+#%%
+# 8. Plots including title stating the coefficient.
 # First it outputs the newly formatted data, you can change the file names
 # the plots will save as.
 # colour_scale.txt is a colour scale specifically made and saved to show 
 # the data but you could make your own or use existing ones from
 # https://ncas-cms.github.io/cf-plot/build/colour_scales.html
 # You will also need to adjust the labels and axis for your region
-cfp.gopen(file="snow-1.png")
+cfp.gopen(rows=1, columns=2, file="snow_and_orog_on_same_grid.png")
 ###cfp.cscale("~/cfplot_data/colour_scale.txt")
+# Joint config
 cfp.mapset(resolution="10m")
-cfp.con(use_snow_normal, 
-        lines=False, 
-        title = "Snow Cover Contour Plot", 
-        xticklabels = ("3W", "2W", "1W"), 
-        yticklabels =("52N", "53N", "54N", "55N"), 
-        xticks = (-3, -2, -1), 
-        yticks= (52, 53, 54, 55))
-cfp.gclose()
+label_info = {
+    "xticklabels": ("3W", "2W", "1W"), 
+    "yticklabels": ("52N", "53N", "54N", "55N"), 
+    "xticks": (-3, -2, -1),
+    "yticks": (52, 53, 54, 55),
+}
 
-#%%
-# 8. Plot of 1km Resolution Orography Contour
-# Here the ocean doesnt get coloured out because when it is regridded it gets 
-# masked by the lack of data for the dependant value (snow) over the seas
-cfp.gopen(file="orog-1.png")
+cfp.gpos(1)
+cfp.con(use_snow_normal,  lines=False, title = "Snow Cover Plot", 
+        **label_info)
+# This plots the regridded file data. The destination grid is the snow grid.
+cfp.gpos(2)
 cfp.cscale("wiki_2_0_reduced")
-cfp.mapset(resolution="10m")
-cfp.con(use_orog, 
-        lines=False, 
-        title = "1km resolution Orography Contour Plot", 
-        xticklabels = ("3W", "2W", "1W"), 
-        yticklabels =("52N", "53N", "54N", "55N"), 
-        xticks = (-3, -2, -1), 
-        yticks= (52, 53, 54, 55))
+cfp.con(reg, lines=False, title="Elevation (1km resolution Orography) Plot",
+        **label_info)
 cfp.gclose()
-
-#%%
-# 9. Plot of Vertical Orography Lineplot of 1km Resolution Elevation
-lonz = use_orog.construct("longitude").data[0]
-elevation_orog = use_orog.subspace(longitude = lonz)
-xticks_elevation_orog = [52, 52.5, 53, 53.5, 54, 54.5, 55]
-
-cfp.gopen(figsize=(12, 6), file ="orography_vertical.png")
-
-cfp.lineplot(
-    x=elevation_orog.coord('latitude').array,
-    y=elevation_orog.array.squeeze(),
-    color="black",
-    title="Orography Profile over part of UK",
-    ylabel="Elevation (m)",
-    xlabel="Latitude",
-    xticks=xticks_elevation_orog,
-)
-
-cfp.plotvars.plot.fill_between(
-    elevation_orog.coord('latitude').array,
-    0,
-    elevation_orog.array.squeeze(),
-    color='black',
-    alpha=0.7
-)
-cfp.gclose()
-
-#%%
-# 10. Regrid the data to have a comparable array.
-# Here the orography file is regridded to the snow file 
-# as snow is higher resolution (if this doesnt work switch them).
-reg = use_orog.regrids(use_snow_normal, method="linear")
-
-
-# This plots the regridded file data
-cfp.gopen(file="regridded-1.png")
-cfp.con(reg, lines=False)
-cfp.gclose()
-
-#%%
-# 11. Squeeze snow data to remove the size 1 axes
-snow_data = snow_data.squeeze()
-
-#%%
-#  12. Final statistical calculations
-coefficient = mstats.pearsonr(reg_data.array, snow_data.array)
-print(f"The Pearson correlation coefficient is {coefficient}")
