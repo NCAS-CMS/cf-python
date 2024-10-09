1543841d366507b620582a86970ff00460bc64c6
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-10-09 22:31:59 +0100

    Update GP recipe 19 to consolidate per-season logic

--- docs/source/recipes/plot_19_recipe.py
+++ docs/source/recipes/plot_19_recipe.py
@@ -27,105 +27,62 @@ am_max = am_max.subspace(T=cf.ge(cf.dt("1980-01-01")))
 am_min = am_min.subspace(T=cf.ge(cf.dt("1980-01-01")))
 print("FINAL FIELDS ARE", am_max, am_min)
 
-am_sub_1 = am_min.collapse("T: mean", group=cf.mam())
-am_sub_2 = am_min.collapse("T: mean", group=cf.jja())
-am_sub_3 = am_min.collapse("T: mean", group=cf.son())
-am_sub_4 = am_min.collapse("T: mean", group=cf.djf())
+# TODO
+colours_seasons_map = {
+    "red": (cf.mam(), "Mean across MAM: March, April and May"),
+    "blue": (cf.jja(), "Mean across JJA: June, July and August"),
+    "green": (cf.son(), "Mean across SON: September, October and November"),
+    "purple": (cf.djf(), "Mean across DJF: December, January and February"),
+}
 
-am_sub_5 = am_max.collapse("T: mean", group=cf.mam())
-am_sub_6 = am_max.collapse("T: mean", group=cf.jja())
-am_sub_7 = am_max.collapse("T: mean", group=cf.son())
-am_sub_8 = am_max.collapse("T: mean", group=cf.djf())
-
-
-cfp.gopen(rows=2, columns=1, bottom=0.2, file="global_avg_sst_plot.png")
+cfp.gopen(
+    rows=2, columns=1, bottom=0.1, top=0.75, file="global_avg_sst_plot.png")
 
 # Put maxima subplot at top since these values are higher, given
 # increasing x axis
-xticks = list(range(1980, 2024))
-xlabels = [None for i in xticks]
-
 cfp.gpos(1)
+plt.suptitle(
+    "Global Average Sea Surface Temperature monthly minima\nand maxima "
+    "including seasonal means of these extrema",
+    fontsize=18,
+)
+for colour, season_query in colours_seasons_map.items():
+    query_on_season, season_description = season_query
+    am_sub = am_max.collapse("T: mean", group=query_on_season)
+    cfp.lineplot(
+        am_sub,
+        color=colour,
+        markeredgecolor=colour,
+        marker="o",
+        xlabel="",
+        label=season_description,
+        title="Maxima per month or season",
+        # TODO FONTSIZE HERE
+    )
 cfp.lineplot(
     am_max,
     color="grey",
     xlabel="",
-    #xticks=xticks,
-    #xticklabels=xlabels,
-)
-cfp.lineplot(
-    am_sub_5,
-    color="red",
-    markeredgecolor="red",
-    marker="o",
-    xlabel="",
-    #xticks=xticks,
-    #xticklabels=xlabels,
-)
-cfp.lineplot(
-    am_sub_6,
-    color="green",
-    markeredgecolor="green",
-    marker="o",
-    xlabel="",
-    #xticks=xticks,
-    #xticklabels=xlabels,
-)
-cfp.lineplot(
-    am_sub_7,
-    color="blue",
-    markeredgecolor="blue",
-    marker="o",
-    xlabel="",
-    #xticks=xticks,
-    #xticklabels=xlabels,
-)
-cfp.lineplot(
-    am_sub_8,
-    color="purple",
-    markeredgecolor="purple",
-    marker="o",
-    xlabel="",
-    #xticks=xticks,
-    #xticklabels=xlabels,
+    ylabel="Temperature (K)",
+    label="All months"
 )
 
 # Minima subplot below the maxima one
 cfp.gpos(2)
+for colour, season_query in colours_seasons_map.items():
+    query_on_season, season_description = season_query
+    am_sub = am_min.collapse("T: mean", group=query_on_season)
+    cfp.lineplot(
+        am_sub,
+        color=colour,
+        markeredgecolor=colour,
+        marker="o",
+        xlabel="",
+        title="Minima per month or season"
+    )
 cfp.lineplot(
     am_min,
     color="grey",
-)
-#cfp.lineplot(
-#    am,
-#    color="blue",
-#    title="Global Average Sea Surface Temperature",
-#    ylabel="Temperature (K)",
-#    xlabel="Time"
-#)
-cfp.lineplot(
-    am_sub_1,
-    color="red",
-    markeredgecolor="red",
-    marker="o"
-)
-cfp.lineplot(
-    am_sub_2,
-    color="green",
-    markeredgecolor="green",
-    marker="o"
-)
-cfp.lineplot(
-    am_sub_3,
-    color="blue",
-    markeredgecolor="blue",
-    marker="o"
-)
-cfp.lineplot(
-    am_sub_4,
-    color="purple",
-    markeredgecolor="purple",
-    marker="o"
 )
 
 cfp.gclose()
