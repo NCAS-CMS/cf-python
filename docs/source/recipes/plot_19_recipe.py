53b981b8669328ee439e129b2d35bb555f48ed12
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-10-09 15:03:14 +0100

    Update GP recipe 19 to find per-season max and min

--- docs/source/recipes/plot_19_recipe.py
+++ docs/source/recipes/plot_19_recipe.py
@@ -17,26 +17,79 @@ f = cf.read("~/recipes_break/ERA5_monthly_averaged_SST.nc")
 sst = f[0]  # Select the SST variable
 
 # Collapse data by area mean (average over spatial dimensions)
-am = sst.collapse("area: mean")  # equivalent to "X Y: mean"
-am.squeeze(inplace=True)
+am_max = sst.collapse("area: maximum")  # equivalent to "X Y: mean"
+am_min = sst.collapse("area: minimum")  # equivalent to "X Y: mean"
+#am.squeeze(inplace=True)cf.seasons()
+print("AM SEASONAL IS", am_min, am_max)
+# REDUCE TO TEST
+am_min = am_min[-100:]  # final 100 points
+am_max = am_max[-100:]  # final 100 points
 
 # Check available coordinates (already found 'dimensioncoordinate0' as the
 # time coordinate)
-print("Available coordinates:", am.coordinates())
+###print("Available coordinates:", am.coordinates())
 
-am_dim_key, am_data = am.coordinate("dimensioncoordinate0", item=True)
-am_sub = am.subspace(**{am_dim_key: cf.mam()})
+###am_dim_key, am_data = am.coordinate("dimensioncoordinate0", item=True)
+am_sub_1 = am_min.collapse("T: mean", group=cf.mam())
+am_sub_2 = am_min.collapse("T: mean", group=cf.jja())
+am_sub_3 = am_min.collapse("T: mean", group=cf.son())
+am_sub_4 = am_min.collapse("T: mean", group=cf.djf())
+am_sub_5 = am_max.collapse("T: mean", group=cf.mam())
+am_sub_6 = am_max.collapse("T: mean", group=cf.jja())
+am_sub_7 = am_max.collapse("T: mean", group=cf.son())
+am_sub_8 = am_max.collapse("T: mean", group=cf.djf())
+
+
+
+"""
+am_sub_1 = am.subspace(**{am_dim_key: cf.mam()})
+am_sub_2 = am.subspace(**{am_dim_key: cf.month(3)})
+am_sub_3 = am.subspace(**{am_dim_key: cf.month(4)})
+am_sub_4 = am.subspace(**{am_dim_key: cf.month(5)})
+am_sub_2 = am_sub_2 - am_sub_1
+am_sub_3 = am_sub_3 - am_sub_1
+am_sub_4 = am_sub_4 - am_sub_1
+"""
+
 
 cfp.gopen(file="global_avg_sst_plot.png")
+#cfp.lineplot(
+#    am,
+#    color="blue",
+#    title="Global Average Sea Surface Temperature",
+#    ylabel="Temperature (K)",
+#    xlabel="Time"
+#)
 cfp.lineplot(
-    am,
-    color="blue",
-    title="Global Average Sea Surface Temperature",
-    ylabel="Temperature (K)",
-    xlabel="Time"
+    am_sub_1,
+    color="red",
 )
 cfp.lineplot(
-    am_sub,
+    am_sub_2,
+    color="green",
+)
+cfp.lineplot(
+    am_sub_3,
+    color="blue",
+)
+cfp.lineplot(
+    am_sub_4,
+    color="purple",
+)
+cfp.lineplot(
+    am_sub_5,
     color="red",
+)
+cfp.lineplot(
+    am_sub_6,
+    color="green",
+)
+cfp.lineplot(
+    am_sub_7,
+    color="blue",
+)
+cfp.lineplot(
+    am_sub_8,
+    color="purple",
 )
 cfp.gclose()
