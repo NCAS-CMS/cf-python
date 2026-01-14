238fd84ca61fb8de345d51bac85c0a0f57dc1496
David Hassell <davidhassell@users.noreply.github.com>
2026-01-14 13:27:07 +0000

    Typos
    Co-authored-by: Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>

--- setup.py
+++ setup.py
@@ -184,7 +184,7 @@ The ``cf`` package can:
 
 * create new field constructs in memory,
 
-* write and append field constructs and domain to netCDF and Zarr v3 datasets on disk,
+* write and append field and domain constructs to netCDF and Zarr v3 datasets on disk,
 
 * read, write, and create coordinates defined by geometry cells,
 
