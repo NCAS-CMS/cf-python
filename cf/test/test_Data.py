8d6ff49c760e90d86503e20018dd388ed837e090
David Hassell <david.hassell@ncas.ac.uk>
2026-01-14 13:54:32 +0000

    _get_cached_elements -> get_cached_elements

--- cf/test/test_Data.py
+++ cf/test/test_Data.py
@@ -1165,7 +1165,7 @@ class DataTest(unittest.TestCase):
         str(d)
         str(e)
         f = cf.Data.concatenate([d, e], axis=1)
-        cached = f._get_cached_elements()
+        cached = f.get_cached_elements()
         self.assertEqual(cached[0], d.first_element())
         self.assertEqual(cached[-1], e.last_element())
 
@@ -1205,7 +1205,7 @@ class DataTest(unittest.TestCase):
         repr(e)
         f = cf.Data.concatenate([d, e], axis=0)
         self.assertEqual(
-            f._get_cached_elements(),
+            f.get_cached_elements(),
             {0: d.first_element(), -1: e.last_element()},
         )
 
@@ -4378,7 +4378,7 @@ class DataTest(unittest.TestCase):
         d = cf.Data([1000, 2000, 3000], "m")
         repr(d)
         d.Units = cf.Units("km")
-        self.assertEqual(d._get_cached_elements(), {0: 1.0, 1: 2.0, -1: 3.0})
+        self.assertEqual(d.get_cached_elements(), {0: 1.0, 1: 2.0, -1: 3.0})
 
     def test_Data_get_data(self):
         """Test the `get_data` Data method."""
@@ -4446,57 +4446,30 @@ class DataTest(unittest.TestCase):
         self.assertTrue((q == d).array.all())
         self.assertTrue((d == q).array.all())
 
-    def test_Data__str__(self):
-        """Test `Data.__str__`"""
-        elements0 = (0, -1, 1)
-        for array in ([1], [1, 2], [1, 2, 3]):
-            elements = elements0[: len(array)]
+    def test_Data__repr__str(self):
+        """Test all means of Data inspection."""
+        for d in [
+            cf.Data(9, units="km"),
+            cf.Data([9], units="km"),
+            cf.Data([[9]], units="km"),
+            cf.Data([8, 9], units="km"),
+            cf.Data([[8, 9]], units="km"),
+            cf.Data([7, 8, 9], units="km"),
+            cf.Data([[7, 8, 9]], units="km"),
+            cf.Data([6, 7, 8, 9], units="km"),
+            cf.Data([[6, 7, 8, 9]], units="km"),
+            cf.Data([[6, 7], [8, 9]], units="km"),
+            cf.Data([[6, 7, 8, 9], [6, 7, 8, 9]], units="km"),
+        ]:
+            _ = repr(d)
+            _ = str(d)
 
-            d = cf.Data(array)
-            cache = d._get_cached_elements()
-            for element in elements:
-                self.assertNotIn(element, cache)
-
-            self.assertEqual(str(d), str(array))
-            cache = d._get_cached_elements()
-            for element in elements:
-                self.assertIn(element, cache)
-
-            d[0] = 1
-            cache = d._get_cached_elements()
-            for element in elements:
-                self.assertNotIn(element, cache)
-
-            self.assertEqual(str(d), str(array))
-            cache = d._get_cached_elements()
-            for element in elements:
-                self.assertIn(element, cache)
-
-            d += 0
-            cache = d._get_cached_elements()
-            for element in elements:
-                self.assertNotIn(element, cache)
-
-            self.assertEqual(str(d), str(array))
-            cache = d._get_cached_elements()
-            for element in elements:
-                self.assertIn(element, cache)
-
-        # Test when size > 3, i.e. second element is not there.
-        d = cf.Data([1, 2, 3, 4])
-        cache = d._get_cached_elements()
-        for element in elements0:
-            self.assertNotIn(element, cache)
-
-        self.assertEqual(str(d), "[1, ..., 4]")
-        cache = d._get_cached_elements()
-        self.assertNotIn(1, cache)
-        for element in elements0[:2]:
-            self.assertIn(element, cache)
-
-        d[0] = 1
-        for element in elements0:
-            self.assertNotIn(element, d._get_cached_elements())
+        # Test when the data contains date-times with the first
+        # element masked
+        dt = np.ma.array([10, 20], mask=[True, False])
+        d = cf.Data(dt, units="days since 2000-01-01")
+        self.assertTrue(str(d) == "[--, 2000-01-21 00:00:00]")
+        self.assertTrue(repr(d) == "<CF Data(2): [--, 2000-01-21 00:00:00]>")
 
     def test_Data_cull_graph(self):
         """Test Data.cull_graph."""
