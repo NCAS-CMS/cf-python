b813522a2cd6e12b29319acdda2c2dfc0f6e1942
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2026-04-10 14:06:14 +0100

    Update dependency version constraints for release

--- Changelog.rst
+++ Changelog.rst
@@ -47,7 +47,7 @@ Version NEXTVERSION
   (https://github.com/NCAS-CMS/cf-python/issues/887)
 * New optional dependency: ``healpix>=2025.1``
 * New dependency: ``pyfive>=1.1.1``
-* Changed dependency: ``cfdm>=1.13.?.?, <1.13.?.0``
+* Changed dependency: ``cfdm>=1.13.1.0, <1.13.2.0``
 
 ----
 
--- cf/__init__.py
+++ cf/__init__.py
@@ -85,16 +85,16 @@ import cfdm
 from packaging.version import Version
 
 
-__date__ = "2026-01-19"
-__version__ = "3.19.0"
+__date__ = "2026-04-10"
+__version__ = "3.20.0"
 __cf_version__ = cfdm.__cf_version__
 __Conventions__ = f"CF-{__cf_version__}"
 
 # Check the version of cfdm (this is worth doing because of the very
 # tight coupling between cf and cfdm, and the risk of bad things
 # happening at run time if the versions are mismatched).
-_minimum_vn = "1.13.0.0"
-_maximum_vn = "1.13.1.0"
+_minimum_vn = "1.13.1.0"
+_maximum_vn = "1.13.2.0"
 _cfdm_vn = Version(cfdm.__version__)
 if _cfdm_vn < Version(_minimum_vn) or _cfdm_vn >= Version(_maximum_vn):
     raise RuntimeError(
