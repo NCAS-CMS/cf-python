7c3205ab9d6f92d6b9e15fcbadd80305e48e6753
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2026-06-01 21:04:29 +0100

    Fix scoping issues in test_HEALPix_utils

--- cf/test/test_HEALPix_utils.py
+++ cf/test/test_HEALPix_utils.py
@@ -22,34 +22,34 @@ class DataTest(unittest.TestCase):
             self.skipTest(
                 "Test module requires 'healpix' package. Install it to run all."
             )
-        else:
-            # Create matching lists of selected nested, ring, nuniq and zuniq
-            # indices for every refinement level.
-            indices = [
-                (r, i, healpix.nest2ring(healpix.order2nside(r), i))
-                for r in range(30)
-                for i in (0, 7, (12 * 4**r) - 1)
-            ]
-            refinement_levels, nested_indices, ring_indices = map(
-                list, zip(*indices)
-            )
 
-            nuniq_indices = [  # noqa: F841
-                i + 4 ** (1 + r)
-                for r, i in zip(refinement_levels, nested_indices)
-            ]
+        # Create matching lists of selected nested, ring, nuniq and zuniq
+        # indices for every refinement level.
+        indices = [
+            (r, i, healpix.nest2ring(healpix.order2nside(r), i))
+            for r in range(30)
+            for i in (0, 7, (12 * 4**r) - 1)
+        ]
+        self.refinement_levels, self.nested_indices, self.ring_indices = map(
+            list, zip(*indices)
+        )
 
-            zuniq_indices = [  # noqa: F841
-                (2 * i + 1) * 4 ** (29 - r)
-                for r, i in zip(refinement_levels, nested_indices)
-            ]
+        self.nuniq_indices = [  # noqa: F841
+            i + 4 ** (1 + r)
+            for r, i in zip(self.refinement_levels, self.nested_indices)
+        ]
+
+        self.zuniq_indices = [  # noqa: F841
+            (2 * i + 1) * 4 ** (29 - r)
+            for r, i in zip(self.refinement_levels, self.nested_indices)
+        ]
 
     def test_HEALPix_uniq2zuniq(self):
         """Test _uniq2zuniq"""
         from cf.data.dask_utils_healpix import _uniq2zuniq
 
         self.assertTrue(
-            np.array_equal(_uniq2zuniq(nuniq_indices), zuniq_indices)
+            np.array_equal(_uniq2zuniq(self.nuniq_indices), self.zuniq_indices)
         )
 
     def test_HEALPix_zuniq2uniq(self):
@@ -57,7 +57,7 @@ class DataTest(unittest.TestCase):
         from cf.data.dask_utils_healpix import _zuniq2uniq
 
         self.assertTrue(
-            np.array_equal(_zuniq2uniq(zuniq_indices), nuniq_indices)
+            np.array_equal(_zuniq2uniq(self.zuniq_indices), self.nuniq_indices)
         )
 
     def test_HEALPix_zuniq2pix(self):
@@ -65,14 +65,14 @@ class DataTest(unittest.TestCase):
         from cf.data.dask_utils_healpix import _zuniq2pix
 
         # nested
-        order, i = _zuniq2pix(zuniq_indices, nest=True)
+        order, i = _zuniq2pix(self.zuniq_indices, nest=True)
 
-        self.assertTrue(np.array_equal(order, refinement_levels))
-        self.assertTrue(np.array_equal(i, nested_indices))
+        self.assertTrue(np.array_equal(order, self.refinement_levels))
+        self.assertTrue(np.array_equal(i, self.nested_indices))
 
         # ring
         with self.assertRaises(NotImplementedError):
-            _zuniq2pix(zuniq_indices, nest=False)
+            _zuniq2pix(self.zuniq_indices, nest=False)
 
     def test_HEALPix_pix2zuniq(self):
         """Test _pix2zuniq"""
@@ -81,18 +81,18 @@ class DataTest(unittest.TestCase):
         # nested
         z = [
             _pix2zuniq(r, i, nest=True)
-            for r, i in zip(refinement_levels, nested_indices)
+            for r, i in zip(self.refinement_levels, self.nested_indices)
         ]
 
-        self.assertTrue(np.array_equal(z, zuniq_indices))
+        self.assertTrue(np.array_equal(z, self.zuniq_indices))
 
         # ring
         z = [
             _pix2zuniq(r, i, nest=False)
-            for r, i in zip(refinement_levels, ring_indices)
+            for r, i in zip(self.refinement_levels, self.ring_indices)
         ]
 
-        self.assertTrue(np.array_equal(z, zuniq_indices))
+        self.assertTrue(np.array_equal(z, self.zuniq_indices))
 
 
 if __name__ == "__main__":
